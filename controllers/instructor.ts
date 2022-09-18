0488c02609cf878f4824d00d8e8ccdbc7ac073da
Enes Ünlüer <enes@kleuize.com>
2022-09-18 07:57:49 +0300

    feat: course model created

--- controllers/instructor.ts
+++ controllers/instructor.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/user";
+import Course from "../models/course";
 import queryString from "query-string";
 import stripe from "stripe";
 
@@ -9,16 +10,13 @@ const stripes = new stripe.Stripe(process.env.STRIPE_SECRET, config);
 export const makeInstructor = async (req: Request, res: Response) => {
   try {
     // 1. find user from db
-    const user = await User.findById(req.body._id).exec();
+    const user = await User.findById(req.body.user._id).exec();
     // 2. if user dont have stripe_account_id yet, then create new
     if (!user.stripe_account_id) {
       const account = await stripes.accounts.create({ type: "express" });
       console.log("ACCOUNT => ", account.id);
       user.stripe_account_id = account.id;
-
-      user.save(function (err) {
-        console.log("test");
-      });
+      user.save();
     }
     let accountLink = await stripes.accountLinks.create({
       account: user.stripe_account_id,
@@ -37,5 +35,57 @@ export const makeInstructor = async (req: Request, res: Response) => {
     res.send(`${accountLink.url}?${queryString.stringify(accountLink)}`);
   } catch (err) {
     console.log("MAKE INSTRUCTOR ERR ", err);
+  }
+};
+
+export const getAccountStatus = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.body.user._id).exec();
+    const account = await stripes.accounts.retrieve(user.stripe_account_id);
+    // console.log("ACCOUNT => ", account);
+    if (!account.charges_enabled) {
+      return res.status(401).send("Unauthorized");
+    } else {
+      const statusUpdated = await User.findByIdAndUpdate(
+        user._id,
+        {
+          stripe_seller: account,
+          $addToSet: { role: "Instructor" },
+        },
+        { new: true }
+      )
+        .select("-password")
+        .exec();
+      res.json(statusUpdated);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentInstructor = async (req: Request, res: Response) => {
+  try {
+    let user = await User.findById(req.body.user._id)
+      .select("-password")
+      .exec();
+    // console.log("CURRENT INSTRUCTOR => ", user);
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      res.json({ ok: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const instructorCourses = async (req:Request, res: Response) => {
+  try {
+    const courses = await Course.find({ instructor: req.body.user._id })
+      .sort({ createdAt: -1 })
+      .exec();
+    res.json(courses);
+  } catch (err) {
+    console.log(err);
   }
 };
---
+++ models/course.ts
@@ -0,0 +1,74 @@
+import { Schema, model, Types } from "mongoose";
+import { ILessonModel, ICourseModel } from "../types";
+
+const lessonSchema = new Schema<ILessonModel>(
+  {
+    title: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlength: 320,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    content: {
+      type: {},
+      minlength: 200,
+    },
+    lecture_quiz: {},
+    lecture_notes: {},
+    lecture_video: {},
+    free_preview: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const courseSchema = new Schema<ICourseModel>(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 3,
+      maxlength: 320,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: {},
+      minlength: 200,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 9.99,
+    },
+    image: {},
+    category: String,
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    paid: {
+      type: Boolean,
+      default: true,
+    },
+    instructor: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lessons: [lessonSchema],
+  },
+  { timestamps: true }
+);
+
+export default model<ICourseModel>("Course", courseSchema);
