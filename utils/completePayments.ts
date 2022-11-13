e5695b56e4f7e88cea7d0f0bbc168e614b5b0a45
Enes Ünlüer <enes@kleuize.com>
2022-11-13 13:37:17 +0300

    feat: completePayment util completed

--- utils/completePayments.ts
+++ utils/completePayments.ts
@@ -1,45 +1,49 @@
 import { Types } from "mongoose";
-
+import Basket from "../models/basket";
 import PaymentFailed from "../models/paymentFailed";
 import PaymentsSuccess from "../models/paymentSuccess";
 
+const { ObjectId } = Types;
 
-const CompletePayment = async result => {
-    if (result?.status === "success") {
-      await _carts.default.updateOne({
-        _id: Types.ObjectId(result?.basketId)
-      }, {
+export const CompletePayment = async (result) => {
+  if (result?.status === "success") {
+    await Basket.updateOne(
+      {
+        //@ts-ignore
+        _id: ObjectId(result?.basketId),
+      },
+      {
         $set: {
-          completed: true
-        }
-      });
-      await PaymentsSuccess.create({
-        status: result.status,
-        cartId: result?.basketId,
-        conversationId: result?.conversationId,
-        currency: result?.currency,
-        paymentId: result?.paymentId,
-        price: result?.price,
-        paidPrice: result?.paidPrice,
-        itemTransactions: result?.itemTransactions.map(item => {
-          return {
-            itemId: item?.itemId,
-            paymentTransactionId: item?.paymentTransactionId,
-            price: item?.price,
-            paidPrice: item?.paidPrice
-          };
-        }),
-        log: result
-      });
-    } else {
-      await PaymentFailed.create({
-        status: result?.status,
-        conversationId: result?.conversationId,
-        errorCode: result?.errorCode,
-        errorMessage: result?.errorMessage,
-        log: result
-      });
-    }
-  };
-  
-  exports.CompletePayment = CompletePayment;
+          completed: true,
+        },
+      }
+    );
+    await PaymentsSuccess.create({
+      status: result.status,
+      cartId: result?.basketId,
+      conversationId: result?.conversationId,
+      currency: result?.currency,
+      paymentId: result?.paymentId,
+      price: result?.price,
+      paidPrice: result?.paidPrice,
+      itemTransactions: result?.itemTransactions.map((item) => {
+        return {
+          itemId: item?.itemId,
+          paymentTransactionId: item?.paymentTransactionId,
+          price: item?.price,
+          paidPrice: item?.paidPrice,
+        };
+      }),
+      log: result,
+    });
+  } else {
+    await PaymentFailed.create({
+      status: result?.status,
+      conversationId: result?.conversationId,
+      errorCode: result?.errorCode,
+      errorMessage: result?.errorMessage,
+      log: result,
+    });
+  }
+};
+
